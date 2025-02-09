4633ac8cc102042ca46a44ce2eb935e0fa5034e1
okaytrip134 <info@okaytrip.in>
2025-02-09 10:40:25 +0530

    Image Compressor

--- NewServer/controllers/packageController.js
+++ NewServer/controllers/packageController.js
@@ -141,7 +141,6 @@ exports.updatePackage = async (req, res) => {
       title: item.title,
       description: item.description,
     }));
-
     // ✅ Optimize Images with sharp
     const images = [];
     for (const file of req.files) {
