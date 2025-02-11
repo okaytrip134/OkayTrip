55ac3fb43c2f2386ab4d3dcc12ef06af7d9ab749
okaytrip134 <info@okaytrip.in>
2025-02-11 15:56:19 +0530

    Category Delete button

--- OkayTrip/src/components/UserAuth.jsx
+++ OkayTrip/src/components/UserAuth.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast"; // Import react-hot-toast
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 const UserAuth = ({ onClose }) => {
-  const API_URL = "http://localhost:8000";
-  const [isLogin, setIsLogin] = useState(true); // Toggle between Login and Signup
+  // const API_URL = "http://localhost:8000";
+  const [isLogin, setIsLogin] = useState(true); 
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -32,6 +33,8 @@ const UserAuth = ({ onClose }) => {
     };
     fetchBannerData();
   }, []);
+  
+  const [showPassword, setShowPassword] = useState(false);
 
 
   const handleInputChange = (e) => {
@@ -41,42 +44,42 @@ const UserAuth = ({ onClose }) => {
   const handleSubmit = async () => {
     try {
       setError(""); // Reset error
-  
+
       const payload = isLogin
         ? { email: formData.email, password: formData.password }
         : {
-            name: formData.name,
-            email: formData.email,
-            password: formData.password,
-            phone: formData.phone,
-          };
-  
+          name: formData.name,
+          email: formData.email,
+          password: formData.password,
+          phone: formData.phone,
+        };
+
       const endpoint = isLogin
         ? `${import.meta.env.VITE_APP_API_URL}/api/user/auth/login`
         : `${import.meta.env.VITE_APP_API_URL}/api/user/auth/register`;
-  
+
       const { data } = await axios.post(endpoint, payload);
-  
+
       console.log("Response from API:", data); // Debugging
-  
+
       if (isLogin) {
         if (!data.token) {
           throw new Error("Login failed: No token received");
         }
-  
+
         localStorage.setItem("userToken", data.token);
         localStorage.setItem("userName", data.user.name);
-  
+
         // Extract expiry from JWT
         const decodedToken = JSON.parse(atob(data.token.split(".")[1]));
         const expiryTime = decodedToken.exp * 1000; // Convert to milliseconds
         localStorage.setItem("tokenExpiry", expiryTime);
-  
+
         console.log("Token Expiry Set:", expiryTime); // Debugging
       } else {
         toast.success("Signup successful!!");
       }
-  
+
       onClose(); // Close the modal on success
     } catch (error) {
       console.error("Login Error:", error);
@@ -92,7 +95,7 @@ const UserAuth = ({ onClose }) => {
 
   return (
     <div className="fixed h-full md:h-auto inset-0 bg-black bg-opacity-20 z-50 flex justify-center items-center">
-       <Toaster position="top-right" /> {/* Toast Notification Container */}
+      <Toaster position="top-right" /> {/* Toast Notification Container */}
       <div className="bg-white rounded-lg shadow-lg w-11/12 max-w-4xl flex overflow-hidden relative">
         {/* Close Button */}
         <button
@@ -106,14 +109,14 @@ const UserAuth = ({ onClose }) => {
         {/* Left Section: Image */}
         <div className="hidden md:block w-1/2 relative">
           <img
-             src={`${import.meta.env.VITE_APP_API_URL}${bannerData.imageUrl}`}
+            src={`${import.meta.env.VITE_APP_API_URL}${bannerData.imageUrl}`}
             alt="Login Illustration"
             className="w-full h-full object-cover"
           />
-            <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black/50 via-black/ to-transparent"></div>
+          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black/50 via-black/ to-transparent"></div>
           <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 flex flex-col justify-end items-start text-left text-white px-4">
-          <h2 className="text-3xl font-bold mb-2">{bannerData.title}</h2>
-          <p className="text-lg">{bannerData.subtitle}</p>
+            <h2 className="text-3xl font-bold mb-2">{bannerData.title}</h2>
+            <p className="text-lg">{bannerData.subtitle}</p>
           </div>
         </div>
 
@@ -127,21 +130,19 @@ const UserAuth = ({ onClose }) => {
           {/* Toggle Login/Signup */}
           <div className="flex justify-center mb-6">
             <button
-              className={`text-lg font-bold px-4 py-2 ${
-                isLogin
+              className={`text-lg font-bold px-4 py-2 ${isLogin
                   ? "text-orange-500 border-b-2 border-orange-500"
                   : "text-gray-500"
-              }`}
+                }`}
               onClick={() => setIsLogin(true)}
             >
               Login
             </button>
             <button
-              className={`text-lg font-bold px-4 py-2 ${
-                !isLogin
+              className={`text-lg font-bold px-4 py-2 ${!isLogin
                   ? "text-orange-500 border-b-2 border-orange-500"
                   : "text-gray-500"
-              }`}
+                }`}
               onClick={() => setIsLogin(false)}
             >
               Sign Up
@@ -178,23 +179,39 @@ const UserAuth = ({ onClose }) => {
               onChange={handleInputChange}
               className="w-full px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
             />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleInputChange}
-              className="w-full px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
-            />
-            {!isLogin && (
+            <div className="relative">
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleInputChange}
+                className="w-full px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
+              />
+              <span
+                className="absolute right-3 top-3 text-gray-500 cursor-pointer"
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                {showPassword ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
+              </span>
+            </div>
+            {!isLogin && (
+              <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
                 name="confirmPassword"
                 placeholder="Confirm Password"
                 value={formData.confirmPassword}
                 onChange={handleInputChange}
                 className="w-full px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
               />
+              <span
+                className="absolute right-3 top-3 text-gray-500 cursor-pointer"
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                {showPassword ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
+              </span>
+            </div>
             )}
           </div>
 
@@ -209,15 +226,15 @@ const UserAuth = ({ onClose }) => {
           </button>
 
           {/* Forgot Password Link */}
-            <div className="mt-4 text-center">
-              <button
-                onClick={handleForgotPassword}
-                className="text-blue-500 text-sm"
-              >
-                Forgot Password?
-              </button>
-            </div>
-  
+          <div className="mt-4 text-center">
+            <button
+              onClick={handleForgotPassword}
+              className="text-blue-500 text-sm"
+            >
+              Forgot Password?
+            </button>
+          </div>
+
 
           {/* Sign in with Google */}
           <button className="w-full mt-4 px-6 py-3 rounded-full bg-gray-100 text-gray-700 font-bold hover:bg-gray-200 flex items-center justify-center">
